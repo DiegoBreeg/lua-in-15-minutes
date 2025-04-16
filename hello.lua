897696a5796c3aabf33372f7ff0dbf0865b38939
diego breeg <diegobreeg@gmail.com>
2025-04-16 00:39:46 -0300

    sss

--- hello.lua
+++ hello.lua
@@ -1,8 +1,18 @@
 string = "teste"
 
 print(string)
-bool = true
+
+local bool = true
 
 if bool then
 	print("bool is true")
 end
+
+-- apenas nil e false são falsos
+
+print("a" == "b")
+-- ~~= diferente de
+-- and e
+-- or ou
+-- == igual a
+--
